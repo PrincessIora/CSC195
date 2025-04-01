1404f822831053f648c6683c6ae2a0086e6ebaed
PrincessIora <pjgatehouse@gmail.com>
2025-04-01 16:05:03 -0600

    New

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
+using namespace std;
+
 int main() 
 {
     std::cout << "Hello world!\n";
     std::cout << "Git Integrated!\n";
+
+    std::string name;
+    std::cout << "Enter name: " << std::endl;
+    std::cin >> name;
+
+    int age;
+    std::cout << "Enter age: " << std::endl;
+    std::cin >> age;
+
+    cout << "Your name is " << name << endl;
+    cout << "Your age is " << age << endl;
+
+
+
 }
