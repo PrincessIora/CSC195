bb2af66a84707a876233374a75a77c96a70dc8c4
PrincessIora <pjgatehouse@gmail.com>
2025-04-10 16:14:21 -0600

    Updated containers example now includes pointer arithmetic, lists, maps

--- Containers/Containers.cpp
+++ Containers/Containers.cpp
@@ -1,14 +1,15 @@
 // Containers.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
 #include <vector>
+#include <list>
+#include <map>
 using namespace std;
 
 int main()
 {
 	string fruits[5] = { "Banana","Kiwi","Mango","Chikoo","Strawberry" }; // example of array with assigned values
-	
+
 	vector<int> numbers;
 	numbers.push_back(1);
 	numbers.push_back(3);
@@ -23,5 +24,57 @@ int main()
 	{
 		cout << numbers[i] << endl;
 	}
+
+	cout << "______________________" << endl;
+
+	//pointer Arithmetic
+
+	string chemicals[5] = { "Mercury", "Hydrochloric Acid","Chlorine","Bromine","Table Salt" };
+	cout << chemicals[2] << endl;
+	cout << chemicals << endl; //returns address of first element of the array
+	cout << &chemicals[0] << endl; //returns address of first element of the array
+
+	cout << "_____________________" << endl;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << chemicals[i] << endl;
+		cout << &chemicals[i] << endl; //returns address of ith element of the array
+	}
+	cout << "_____________________" << endl;
+
+	string* p = chemicals;
+	cout << p << endl;
+	cout << ++p << endl;
+	cout << ++p << endl;
+	cout << --p << endl;
+	cout << p + 2 << endl;
+	cout << p - 3 << endl;
+
+	cout << "_____________________" << endl;
+
+	//containers
+	list<string> cars = { "Hyundai", "Ferrari", "Mercedes", "Volvo" };
+
+	cout << cars.front() << endl; //returns first element of the list
+	cout << cars.back() << endl; //returns last element of the list
+
+	cars.pop_back(); //removes last element of the list
+	cout << cars.back() << endl; 
+
+	cars.push_front("Tesla");//adds element at the front of the list
+	cout << cars.front() << endl; 
+
+	cout << "_____________________" << endl;
+
+	map<string, int> pet = { {"Dogs",23},{"Chameleons",3},{"Turtle",43} };
+
+	pet["Dogs"] = 50;
+	cout << pet["Dogs"] << endl;
+
+	pet["Spiders"] = 2;//adds new element to the map
+
+	cout << pet.empty() << endl; //returns 0 if map is not empty
+
 
 }
